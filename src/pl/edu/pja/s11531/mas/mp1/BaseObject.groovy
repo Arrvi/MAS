4e58fd34f7201b0d1722baa6084d797873b13b24
Kristian Kann <arrvi@arrvi.eu>
2016-11-05 18:45:03 +0100

    Initial commit - Base object

---
+++ src/pl/edu/pja/s11531/mas/mp1/BaseObject.groovy
@@ -0,0 +1,12 @@
+package pl.edu.pja.s11531.mas.mp1
+
+/**
+ * Created by kris on 05.11.16.
+ */
+abstract class BaseObject {
+    private static Map<Class<? extends BaseObject>, List<? extends BaseObject>> extent = new HashMap<>();
+
+    protected static addObject(BaseObject baseObject) {
+
+    }
+}
