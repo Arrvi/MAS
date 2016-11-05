0cacfe76156084c504e8d15304522ee22ed99744
Kristian Kann <arrvi@arrvi.eu>
2016-11-05 19:59:49 +0100

    BaseObject extent store

--- src/pl/edu/pja/s11531/mas/mp1/BaseObject.groovy
+++ src/pl/edu/pja/s11531/mas/mp1/BaseObject.groovy
@@ -3,10 +3,28 @@ package pl.edu.pja.s11531.mas.mp1
 /**
  * Created by kris on 05.11.16.
  */
-abstract class BaseObject {
-    private static Map<Class<? extends BaseObject>, List<? extends BaseObject>> extent = new HashMap<>();
+abstract class BaseObject implements Serializable {
+    private static Map<Class<? extends BaseObject>, List<BaseObject>> extent = [:].withDefault {[]};
 
-    protected static addObject(BaseObject baseObject) {
+    protected static addObject(BaseObject object) {
+        extent[object.class].add object
+    }
 
+    public static <C extends BaseObject> List<C> getExtent(Class<C> cls) {
+        return (List<C>) extent[cls];
+    }
+
+    public static saveAll(File store) {
+        def stream = store.newObjectOutputStream()
+        extent.each {
+            it.getValue().each stream.&leftShift
+        }
+        stream.close()
+    }
+
+    public static loadAll(File store) {
+        def stream = store.newObjectInputStream()
+        stream.eachObject BaseObject.&addObject
+        stream.close()
     }
 }
